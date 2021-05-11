3eef32bdf140596b2fdaa05ddb3c24ed2db602ae
Abhinn Mishra <aviabhinn98@gmail.com>
2021-05-11 11:17:38 +0530

    unit testing

---
+++ ACM.BL/Tests/ACM.BL.Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BL.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //Arrange
+
+            Customer customer = new Customer() {
+                FirstName = "Abhinn",
+                LastName = "Mishra"
+            };
+            string expected = "Mishra, Abhinn";
+
+            //Act
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
